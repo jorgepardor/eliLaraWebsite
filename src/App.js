d5ecccaf3bc79187aa02684e65150e12ee6a345a
Jorge P <hi@jorgepardo.dev>
2022-06-19 19:56:58 +0200

    Added home

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <h1 className=' text-lime-400'>Hello World</h1>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header>
+				<div>
+					<img
+						className="w-1/2"
+						src="https://res.cloudinary.com/elizapardo/image/upload/v1655661179/logo_main_qhkbur.webp"
+						alt="logo"
+					/>
+
+					<h1>Under development</h1>
+				</div>
+			</header>
+		</div>
+	);
 }
 
 export default App;
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,135 +2,100 @@ import React from "react";
 
 export const Navbar = () => {
 	return (
-		// <div className="navbar bg-a-main-yellow">
-		//     {/* Menú del modo para pantallas de móvil. */}
-		//     <div className="navbar-start">
-		//         <div className="dropdown">
-		//             <label tabindex="0" className="btn btn-ghost lg:hidden">
-		//                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-		//             </label>
-		//             <ul tabindex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-		//                 <li><a href="/">Home</a></li>
-		//                 <li><a href="/editorial">Diseño Editorial</a></li>
-		//                 <li><a href="/corporativo">Imagen Corporativa</a></li>
-		//                 <li><a href="/marketing">Marketing</a></li>
-		//                 <li><a href="/3d">Cinema 4D</a></li>
-		//                 <li><a href="/personal">Proyectos Personales</a></li>
-		//                 <li><a href="/video">Edición de Vídeo</a></li>
-		//                 <li><a href="/contacto">Contacto</a></li>
-		//             </ul>
-		//         </div>
-		//         {/* <a className="btn btn-ghost normal-case text-xl">daisyUI</a> */}
-		//     </div>
-
-		//     {/* Menú principal de la página */}
-		//     <div className="navbar-center hidden lg:flex">
-		//         <ul className="menu menu-horizontal p-0">
-		//         <li><a className="text-a-main-dark hover:text-a-main-light" href="/">Home</a></li>
-		//         <li><a className="text-a-main-dark hover:text-a-main-light" href="/editorial">Diseño Editorial</a></li>
-		//         <li><a className="text-a-main-dark hover:text-a-main-light" href="/corporativo">Imagen Corporativa</a></li>
-		//         <li><a className="text-a-main-dark hover:text-a-main-light" href="/marketing">Marketing</a></li>
-		//         <li><a className="text-a-main-dark hover:text-a-main-light" href="/3d">Cinema 4D</a></li>
-		//         <li><a className="text-a-main-dark hover:text-a-main-light" href="/personal">Proyectos Personales</a></li>
-		//         <li><a className="text-a-main-dark hover:text-a-main-light" href="/video">Edición de Vídeo</a></li>
-		//         <li><a className="text-a-main-dark hover:text-a-main-light" href="/contacto">Contacto</a></li>
-		//         </ul>
-		//     </div>
-		//     <div className="navbar-end">
-		//         {/* <a className="btn">Get started</a> */}
-		//     </div>
-		// </div>
-
-		<div class=" bg-a-main-medium">
-			<div class="navbar-start">
-				<div class="dropdown">
-					<label tabindex="0" class="btn btn-ghost lg:hidden">
+		<nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
+			<div class="container flex flex-wrap justify-between items-center mx-auto">
+				<a href="https://flowbite.com" class="flex items-center">
+					<img
+						src="https://res.cloudinary.com/elilara/image/upload/v1652119239/logo_durqdc.png"
+						class="mr-3 h-6 sm:h-9"
+						alt="Flowbite Logo"
+					/>
+					<span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+						Flowbite
+					</span>
+				</a>
+				<div class="flex md:order-2">
+					<button
+						type="button"
+						class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+					>
+						Get started
+					</button>
+					<button
+						data-collapse-toggle="mobile-menu-4"
+						type="button"
+						class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+						aria-controls="mobile-menu-4"
+						aria-expanded="false"
+					>
+						<span class="sr-only">Open main menu</span>
 						<svg
+							class="w-6 h-6"
+							fill="currentColor"
+							viewBox="0 0 20 20"
 							xmlns="http://www.w3.org/2000/svg"
-							class="h-5 w-5"
-							fill="none"
-							viewBox="0 0 24 24"
-							stroke="currentColor"
 						>
 							<path
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								stroke-width="2"
-								d="M4 6h16M4 12h8m-8 6h16"
-							/>
+								fill-rule="evenodd"
+								d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+								clip-rule="evenodd"
+							></path>
 						</svg>
-					</label>
-					<ul
-						tabindex="0"
-						class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
-					>
+						<svg
+							class="hidden w-6 h-6"
+							fill="currentColor"
+							viewBox="0 0 20 20"
+							xmlns="http://www.w3.org/2000/svg"
+						>
+							<path
+								fill-rule="evenodd"
+								d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+								clip-rule="evenodd"
+							></path>
+						</svg>
+					</button>
+				</div>
+				<div
+					class="hidden justify-between items-center w-full md:flex md:w-auto md:order-1"
+					id="mobile-menu-4"
+				>
+					<ul class="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
 						<li>
-							<a>Item 1</a>
-						</li>
-						<li tabindex="0">
-							<a class="justify-between">
-								Parent
-								<svg
-									class="fill-current"
-									xmlns="http://www.w3.org/2000/svg"
-									width="24"
-									height="24"
-									viewBox="0 0 24 24"
-								>
-									<path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
-								</svg>
+							<a
+								href="..."
+								class="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
+								aria-current="page"
+							>
+								Home
 							</a>
-							<ul class="p-2">
-								<li>
-									<a>Submenu 1</a>
-								</li>
-								<li>
-									<a>Submenu 2</a>
-								</li>
-							</ul>
 						</li>
 						<li>
-							<a>Item 3</a>
+							<a
+								href="..."
+								class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+							>
+								About
+							</a>
+						</li>
+						<li>
+							<a
+								href="..."
+								class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+							>
+								Services
+							</a>
+						</li>
+						<li>
+							<a
+								href="..."
+								class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+							>
+								Contact
+							</a>
 						</li>
 					</ul>
 				</div>
-				<a class="btn btn-ghost normal-case text-xl">daisyUI</a>
 			</div>
-			<div class="navbar-center hidden lg:flex">
-				<ul class="menu menu-horizontal p-0">
-					<li>
-						<a>Item 1</a>
-					</li>
-					<li tabindex="0">
-						<a>
-							Parent
-							<svg
-								class="fill-current"
-								xmlns="http://www.w3.org/2000/svg"
-								width="20"
-								height="20"
-								viewBox="0 0 24 24"
-							>
-								<path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
-							</svg>
-						</a>
-						<ul class="p-2">
-							<li>
-								<a>Submenu 1</a>
-							</li>
-							<li>
-								<a>Submenu 2</a>
-							</li>
-						</ul>
-					</li>
-					<li>
-						<a>Item 3</a>
-					</li>
-				</ul>
-			</div>
-			<div class="navbar-end">
-				<a class="btn">Get started</a>
-			</div>
-		</div>
+		</nav>
 	);
 };
