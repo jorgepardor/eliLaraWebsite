c48dcdfc41ad0509fc6e44206ffbcc24eea768bd
Jorge P <hi@jorgepardo.dev>
2022-07-10 19:40:07 +0200

    Video en el main page y correcciones finales en el navbar

--- src/App.js
+++ src/App.js
@@ -2,14 +2,23 @@ import "./App.css";
 
 function App() {
 	return (
-		<div className="App grow"> 
-			<header className="...">
+		<div className="App grow">
+			<header className="flex h-96">
 
-					<img
-						className="mt-60 w-2/3 md:w-1/3 lg:2/5 m-auto max-w-lg"
-						src="https://res.cloudinary.com/elizapardo/image/upload/v1655667645/Artboard-1-copy_eewqjy.webp"
-						alt="logo"
+				<div className="mt-48 mx-auto">
+					<video
+						className="m-auto"
+						src="https://res.cloudinary.com/elizapardo/video/upload/v1657467828/ElizabethLaraMainVideo_ahiiws.mp4"
+						autoPlay
+						muted
 					/>
+				</div>
+
+				{/* <img
+					className="mt-60 w-2/3 md:w-1/3 lg:2/5 m-auto max-w-lg"
+					src="https://res.cloudinary.com/elizapardo/image/upload/v1655667645/Artboard-1-copy_eewqjy.webp"
+					alt="logo"
+				/> */}
 
 			</header>
 		</div>
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,121 +2,80 @@ import React, { useState } from "react";
 
 export const Navbar = () => {
 
-	const [menuToggle, setMenuToggle] = useState(false);
+	// const [menuToggle, setMenuToggle] = useState(false);
 
 
+	const [navbarOpen, setNavbarOpen] = useState(false);
 	return (
-
-		<nav className=" bg-Purple-dark  px-2 sm:px-4 py-2.5  dark:bg-gray-800">
-			<div className="container flex justify-between items-center mx-auto">
-				<a href="/" className="flex items-center">
+		<>
+			<nav className="relative flex flex-wrap items-center justify-between px-2 py-3 bg-Purple-dark mb-3">
+				<div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
+					<div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
 					<img
-						src="https://res.cloudinary.com/elizapardo/image/upload/v1656870897/logotokenbw_cqvis3.webp"
-						className="mr-3 h-6 sm:h-9"
+						src="https://res.cloudinary.com/elizapardo/video/upload/v1657474724/MainVideoV2.mp4"
+						className="mr-3 h-9"
 						alt="Elizabeth Lara"
 					/>
-				</a>
-				<div className="flex md:order-2">
-					<button
-						type="button"
-						className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-Light-gray dark:focus:ring-gray-600"
-						aria-controls="mobile-menu"
-						aria-expanded="false"
-						id="mobileMenuToggle"
-						onClick={() => setMenuToggle(!menuToggle)}
-					>
-						<span className="sr-only">Open main menu</span>
-						<svg
-							className="w-6 h-6"
-							fill="currentColor"
-							viewBox="0 0 20 20"
-							xmlns="http://www.w3.org/2000/svg"
+						<button
+							className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+							type="button"
+							onClick={() => setNavbarOpen(!navbarOpen)}
 						>
-							<path
-								fillRule="evenodd"
-								d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-								clipRule="evenodd"
-							></path>
-						</svg>
-						<svg
-							className="hidden w-6 h-6"
-							fill="currentColor"
-							viewBox="0 0 20 20"
-							xmlns="http://www.w3.org/2000/svg"
-						>
-							<path
-								fillRule="evenodd"
-								d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-								clipRule="evenodd"
-							></path>
-						</svg>
-					</button>
-
-					<a
-						href="/contacto"
-						type="button"
-						className="hidden md:block font-Mukta text-Yellow-custom bg-Purple-dark hover:bg-Purple-light focus:ring-4 focus:outline-none focus:ring-Purple-dark font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0"
+							<span className="font-Mukta text-2xl">☰</span>
+						</button>
+					</div>
+					<div
+						className={
+							"lg:flex flex-grow items-center" +
+							(navbarOpen ? " flex" : " hidden")
+						}
+						id="example-navbar-danger"
 					>
-						Contacto
-					</a>
-
+						<ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
+							<li className="nav-item">
+								<a
+									className="px-3 py-2 flex items-center text-xs leading-snug text-white hover:opacity-75"
+									href="/editorial"
+								>
+									<span className="font-Mukta text-sm">Diseño Editorial</span>
+								</a>
+							</li>
+							<li className="nav-item">
+								<a
+									className="px-3 py-2 flex items-center text-xs leading-snug text-white hover:opacity-75"
+									href="/corporativo"
+								>
+									<span className="font-Mukta text-sm">Imagen Corporativa</span>
+								</a>
+							</li>
+							<li className="nav-item">
+								<a
+									className="px-3 py-2 flex items-center text-xs leading-snug text-white hover:opacity-75"
+									href="/marketing"
+								>
+									<span className="font-Mukta text-sm">Marketing</span>
+								</a>
+							</li>
+							<li className="nav-item">
+								<a
+									className="px-3 py-2 flex items-center text-xs leading-snug text-white hover:opacity-75"
+									href="/3d"
+								>
+									<span className="font-Mukta text-sm">Cinema 4D</span>
+								</a>
+							</li>
+							<li className="nav-item">
+								<a
+									className="px-3 py-2 flex items-center text-xs leading-snug text-white hover:opacity-75"
+									href="/video"
+								>
+									<span className="font-Mukta text-sm">Edición de Vídeo</span>
+								</a>
+							</li>
+						</ul>
+					</div>
 				</div>
-				<div
-					className={"justify-between items-center w-full md:flex md:w-auto md:order-1 " + (menuToggle ? '' : 'hidden')}
-					id="mobile-menu"
-				>
-					<ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
-						<li>
-							<a
-								href="/editorial"
-								className="font-Mukta text-sm block py-2 pr-4 pl-3 text-Light-gray border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-Yellow-custom md:p-0 dark:hover:text-white"
-							>
-								Diseño Editorial
-							</a>
-						</li>
-						<li>
-							<a
-								href="/corporativo"
-								className="font-Mukta text-sm block py-2 pr-4 pl-3 text-Light-gray border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-Yellow-custom md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-Light-gray dark:hover:text-white md:dark:hover:bg-transparent dark:border-Light-gray"
-							>
-								Imagen Corporativa
-							</a>
-						</li>
-						<li>
-							<a
-								href="/marketing"
-								className="font-Mukta text-sm block py-2 pr-4 pl-3 text-Light-gray border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-Yellow-custom md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-Light-gray dark:hover:text-white md:dark:hover:bg-transparent dark:border-Light-gray"
-							>
-								Marketing
-							</a>
-						</li>
-						<li>
-							<a
-								href="/3d"
-								className="font-Mukta text-sm block py-2 pr-4 pl-3 text-Light-gray border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-Yellow-custom md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-Light-gray dark:hover:text-white md:dark:hover:bg-transparent dark:border-Light-gray"
-							>
-								Cinema 4D
-							</a>
-						</li>
-						<li>
-							<a
-								href="/personal"
-								className="font-Mukta text-sm block py-2 pr-4 pl-3 text-Light-gray border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-Yellow-custom md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-Light-gray dark:hover:text-white md:dark:hover:bg-transparent dark:border-Light-gray"
-							>
-								Proyectos Personales
-							</a>
-						</li>
-						<li>
-							<a
-								href="/video"
-								className="font-Mukta text-sm block py-2 pr-4 pl-3 text-Light-gray border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-Yellow-custom md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-Light-gray dark:hover:text-white md:dark:hover:bg-transparent dark:border-Light-gray"
-							>
-								Edición de Vídeo
-							</a>
-						</li>
-					</ul>
-				</div>
-			</div>
-		</nav>
+			</nav>
+		</>
 	);
 };
