48b7e0e3178d37ead1d6fabd11ef3d1b9ff2433f
Jorge Pardo <hi@jorgepardo.dev>
2022-11-13 19:35:39 +0100

    Codec de video configurado para que funcione en webkit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import "./App.css";
 
 function App() {
-	return (
-		<div className="flex h-screen justify-center items-center">
-			<video
-				className="m-auto"
-				src="https://res.cloudinary.com/elizapardo/video/upload/v1668003184/ElizabethOpeningV2_nqqacn.mp4"
-				autoPlay
-				muted
-			/>
-		</div>
-	);
+  return (
+    <div className="flex h-screen justify-center items-center">
+      <video
+        className="m-auto"
+		src="https://res.cloudinary.com/elizapardo/video/upload/vc_h264:main:3.1/v1668003184/ElizabethOpeningV2_nqqacn.mp4"
+		type="video/mp4"
+        autoPlay
+        muted
+      />
+    </div>
+  );
 }
 
 export default App;
